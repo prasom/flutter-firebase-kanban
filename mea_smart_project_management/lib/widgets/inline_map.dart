20dab49aa4cee5a90215263387c95521d97d2c12
Pornsak Prasom <pornsak.prasom@allianz.com>
2019-07-27 13:39:14 +0700

    feat:add map view

--- mea_smart_project_management/lib/widgets/inline_map.dart
+++ mea_smart_project_management/lib/widgets/inline_map.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_native_web/flutter_native_web.dart';
+import 'package:flutter_native_web/flutter_native_web.dart';
 
 class InlineMapWidget extends StatefulWidget {
   final String lat;
@@ -15,7 +15,7 @@ class InlineMapWidget extends StatefulWidget {
 }
 
 class _InlineMapWidgetState extends State<InlineMapWidget> {
-  // WebController webController;
+  WebController webController;
 
   @override
   void initState() {
@@ -29,14 +29,14 @@ class _InlineMapWidgetState extends State<InlineMapWidget> {
 
   @override
   Widget build(BuildContext context) {
-    // FlutterNativeWeb flutterWebView = new FlutterNativeWeb(
-    //   onWebCreated: onWebCreated,
-    // );
+    FlutterNativeWeb flutterWebView = new FlutterNativeWeb(
+      onWebCreated: onWebCreated,
+    );
     return Container(
       child: new Column(
         children: <Widget>[
           new Container(
-            // child: flutterWebView,
+            child: flutterWebView,
             height: 300.0,
             width: 500.0,
           ),
@@ -45,16 +45,16 @@ class _InlineMapWidgetState extends State<InlineMapWidget> {
     );
   }
 
-  // void onWebCreated(webController) {
-  //   final _lat = widget.lat;
-  //   final _long = widget.long;
-  //   final _color = widget.colorCode;
-  //   this.webController = webController;
-  //   this.webController.loadUrl("https://gisapi.mea.or.th/embed/landmark?q=$_lat,$_long&pinColor=$_color&z=17&mt=standard&s=true&key=c3c6cdcaf6c320f09150ccc66a764d63&latlonOnly=true");
-  //   this.webController.onPageStarted.listen((url) => print("Loading $url"));
-  //   this
-  //       .webController
-  //       .onPageFinished
-  //       .listen((url) => print("Finished loading $url"));
-  // }
+  void onWebCreated(webController) {
+    final _lat = widget.lat;
+    final _long = widget.long;
+    final _color = widget.colorCode;
+    this.webController = webController;
+    this.webController.loadUrl("https://gisapi.mea.or.th/embed/landmark?q=$_lat,$_long&pinColor=$_color&z=17&mt=standard&s=true&key=c3c6cdcaf6c320f09150ccc66a764d63&latlonOnly=true");
+    this.webController.onPageStarted.listen((url) => print("Loading $url"));
+    this
+        .webController
+        .onPageFinished
+        .listen((url) => print("Finished loading $url"));
+  }
 }
