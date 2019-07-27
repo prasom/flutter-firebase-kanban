0daf416e11ae0bacbab653d547355f353db89b40
Pornsak Prasom <pornsak.prasom@allianz.com>
2019-07-27 20:27:39 +0700

    fix map

--- mea_smart_project_management/lib/widgets/inline_map.dart
+++ mea_smart_project_management/lib/widgets/inline_map.dart
@@ -50,11 +50,17 @@ class _InlineMapWidgetState extends State<InlineMapWidget> {
     final _long = widget.long;
     final _color = widget.colorCode;
     this.webController = webController;
-    this.webController.loadUrl("https://gisapi.mea.or.th/embed/landmark?q=$_lat,$_long&pinColor=$_color&z=17&mt=standard&s=true&key=c3c6cdcaf6c320f09150ccc66a764d63&latlonOnly=true");
+    // this.webController.loadUrl("http://google.co.th");
+    try {
+       this.webController.loadUrl("https://gisapi.mea.or.th/embed/landmark?q=$_lat,$_long&pinColor=$_color&z=17&mt=standard&s=true&key=c3c6cdcaf6c320f09150ccc66a764d63&latlonOnly=true");
     this.webController.onPageStarted.listen((url) => print("Loading $url"));
     this
         .webController
         .onPageFinished
         .listen((url) => print("Finished loading $url"));
+    } catch (e) {
+      print("Error loading $e");
+    }
+   
   }
 }
